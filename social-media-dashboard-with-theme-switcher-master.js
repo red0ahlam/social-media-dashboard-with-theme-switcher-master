e719233b36f9d0bfd7f3105e65e317ee3ac61668
red0ahlam <109111970+red0ahlam@users.noreply.github.com>
2022-12-06 12:39:32 +0100

    Update social-media-dashboard-with-theme-switcher-master.js

--- social-media-dashboard-with-theme-switcher-master.js
+++ social-media-dashboard-with-theme-switcher-master.js
@@ -1,65 +1,33 @@
 
-if(window.localStorage.getItem('theme')==''){
-    window.localStorage.clear();
-    window.localStorage.setItem('theme','white');
-}
+let theme = window.localStorage.getItem('theme');
+const toggle_button = document.querySelector(".toggle-button");
+const body = document.querySelector(".body");
 
-var theme = window.localStorage.getItem('theme');
-
-var root = document.querySelector(':root');
-var root_styles = getComputedStyle(root);
-var toggle_button = document.querySelector('.toggle-button');
-
-function style(theme){
-    var bg = '--clr-'+theme+'-bg';
-    var bg_value = root_styles.getPropertyValue(bg);
-    var top_bg = '--clr-'+theme+'-top-bg';
-    var top_bg_value = root_styles.getPropertyValue(top_bg);
-    var card_bg = '--clr-'+theme+'-card-bg';
-    var card_bg_value = root_styles.getPropertyValue(card_bg);
-    var card_hover_bg = '--clr-'+theme+'-card-hover-bg';
-    var card_hover_bg_value = root_styles.getPropertyValue(card_hover_bg);
-    var text_accent = '--clr-'+theme+'-text-accent';
-    var text_accent_value = root_styles.getPropertyValue(text_accent);
-    var text_primary = '--clr-'+theme+'-text-primary';
-    var text_primary_value = root_styles.getPropertyValue(text_primary);
-   
-    root.style.setProperty('--clr-bg', bg_value);
-    root.style.setProperty('--clr-top-bg', top_bg_value);
-    root.style.setProperty('--clr-card-bg', card_bg_value);
-    root.style.setProperty('--clr-card-hover-bg', card_hover_bg_value);
-    root.style.setProperty('--clr-text-accent', text_accent_value);
-    root.style.setProperty('--clr-text-primary', text_primary_value);
-}
-
-function toggle () {
-
-    if (theme == 'dark') {
-        theme = 'white';
-        style(theme);
-        window.localStorage.setItem("theme", "white");
-        toggle_button.classList.remove('dark-button');
-        toggle_button.classList.add('white-button');
-        toggle_button.ariaPressed = "true";
-    }else {
-        theme = 'dark';
-        style(theme);
-        window.localStorage.setItem("theme", "dark");
-        toggle_button.classList.remove('white-button');
-        toggle_button.classList.add('dark-button');
-        toggle_button.ariaPressed = "false";
-    }
-}
-
-toggle_button.addEventListener('click', toggle);
-style(theme);
-
-if (theme == 'dark') {
+function enable() {
+    body.classList.add("dark-mode");
+    toggle_button.ariaPressed = "true";
     toggle_button.classList.add('dark-button');
     toggle_button.classList.remove('white-button');
-    toggle_button.ariaPressed = "true";
-}else {
+    window.localStorage.setItem("theme", "dark");
+}
+
+function disable() {
+    body.classList.remove("dark-mode");
+    toggle_button.ariaPressed = "false";
     toggle_button.classList.add('white-button');
     toggle_button.classList.remove('dark-button');
-    toggle_button.ariaPressed = "false";
+    window.localStorage.setItem("theme", "white");
 }
+
+if (theme == "dark") {
+    enable();
+}
+
+toggle_button.addEventListener('click', () => {
+    let theme = window.localStorage.getItem('theme');
+    if (theme == "white") {
+        enable();
+    }else {
+        disable();
+   }
+})
